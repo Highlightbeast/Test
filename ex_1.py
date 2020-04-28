6c84e11e94788c1513eaa7d310477cb9627d3a93
Highlightbeast <49416769+Highlightbeast@users.noreply.github.com>
2020-04-28 20:21:20 +0200

    Update ex_1.py
    adjust the width of bars

--- ex_1.py
+++ ex_1.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
+from scipy.stats import norm
 from mpl_toolkits.mplot3d import Axes3D
 # 1000 samples from an uni-variant gaussian distribution with mean 1 and a standard deviation of 0.2
 mu = 1
-sigma = 0.1
+sigma = 0.2
 n_bins = 30
 # experimental distribution
 samples = np.sort(np.random.normal(mu, sigma, size=1000))
@@ -16,10 +17,10 @@ ax1 = fig1.add_subplot(121)
 # bins: The edges of the bins, edges = n_bins + 1
 n, bins, patches = ax1.hist(samples, n_bins, density=1)
 # Ground truth distribution
-y = ((1 / (np.sqrt(2 * np.pi) * sigma)) * np.exp(-0.5 * ((samples - mu) / sigma)**2))
+x = np.arange(mu-3*sigma,mu+3*sigma,0.001)
+# y = ((1 / (np.sqrt(2 * np.pi) * sigma)) * np.exp(-0.5 * ((samples - mu) / sigma)**2))
 ax2 = fig1.add_subplot(122)
-ax2.plot(samples, y, label='ground truth distribution')
-plt.show()
+ax2.plot(x, norm.pdf(x, mu, sigma), label='ground truth distribution')
 
 # 2D
 mean_vec = np.array([0.5, -0.2])
@@ -35,19 +36,17 @@ pos[:, :, 1] = y
 rv = multivariate_normal(mean_vec, cov)
 # z axis is the joint pdf of random variables
 z = rv.pdf(pos)
-fig2 = plt.figure()
-ax3 = plt.axes(projection='3d')
+fig2 = plt.figure(figsize=(9, 5))
+ax3 = fig2.add_subplot(121,projection='3d')
 ax3.plot_wireframe(x, y, z)
 ax3.set_xlabel('x')
 ax3.set_ylabel('y')
 ax3.set_zlabel('PDF')
 ax3.set_title('Ground Truth Distribution 2D')
-plt.show()
 
-fig3 = plt.figure()
-ax4 = fig3.gca(projection='3d')
+ax4 = fig2.add_subplot(122,projection='3d')
 z = np.random.multivariate_normal(mean_vec, cov, 10000)
-hist, xedges, yedges = np.histogram2d(z[:, 0], z[:, 1], bins=30, range=[[-6, 6], [-3, 2]], density=True)
+hist, xedges, yedges = np.histogram2d(z[:, 0], z[:, 1], bins=30, range=[[-6, 6], [-3, 3]], density=True)
 # :-1 except the last one, since it is not included
 x_pos, y_pos = np.meshgrid(xedges[:-1], yedges[:-1])
 # x_pos = x_pos.flatten('F')
@@ -58,9 +57,13 @@ y_pos = y_pos.ravel()
 z_pos = np.zeros_like(x_pos)
 
 # Construct arrays with the dimensions for the 100 bars.
-dx = np.ones_like(z_pos)
-dy = dx.copy()
+# Note that the width of bars should be identical to the distance between two subsequential samples
+# dx = np.ones_like(z_pos)
+# dy = dx.copy()
+dx =  xedges[1] - xedges[0]
+dy =  yedges[1] - yedges[0]
 dz = hist.flatten()
 
 ax4.bar3d(x_pos, y_pos, z_pos, dx, dy, dz, color='g', zsort='average')
 plt.show()
+
